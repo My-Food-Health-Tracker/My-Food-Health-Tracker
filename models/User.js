fb68884a1d464f64b34fdad423ee807815657f2b
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-12-09 18:43:01 +0100

    models

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  password: String,
+  history: [{ type: Schema.Types.ObjectId, ref: 'Day' }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
