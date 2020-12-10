c0910b6d4ccbfab2fbbea22d265787f152bde049
Facundo Gomez <gomezvera.f@gmail.com>
2020-12-10 11:27:41 +0100

    create test view

---
+++ client/src/components/TestView.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+
+  state = {
+
+  }
+
+// getData = () => {
+//   Axios.get('localhost/')
+//   .then
+//   // set data to state
+// }
+
+  render() {
+    return (
+      <div>
+          Get and show data by whatever parameter
+
+          Post to create a piece of data
+      </div>
+    )
+  }
+}
