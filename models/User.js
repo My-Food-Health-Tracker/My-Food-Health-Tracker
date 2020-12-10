ef15935d1def80b39721da429a57ff85fa2121f7
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-12-10 10:15:16 +0100

    add email property in User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
+  email: String,
   password: String,
   history: [{ type: Schema.Types.ObjectId, ref: 'Day' }],
 });
